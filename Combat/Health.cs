ad14194d9561d3072697d9da5b50cb1274db9fa4
katiepillar6 <44276821+katiepillar6@users.noreply.github.com>
2019-05-09 22:21:39 -0700

    Add files via upload

---
+++ Combat/Health.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Combat
+{
+
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float healthPoints = 100f;
+
+        bool isDead = false;
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
+
+        public void TakeDamage(float damage)
+        {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            
+            if (healthPoints == 0)
+            {
+                Death();
+            }
+        }
+
+        public void Death()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
+            
+        }
+    }
+}
