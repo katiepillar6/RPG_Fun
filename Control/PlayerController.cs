51bad9143b79c00bdcbc88e5f7f2de655bd54a2f
katiepillar6 <44276821+katiepillar6@users.noreply.github.com>
2019-05-09 22:19:38 -0700

    Add files via upload

---
+++ Control/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+using System;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+    // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+    // Update is called once per frame
+        void Update()
+        {
+            if(InteractWithCombat()) return;
+            if(InteractWithMovement()) return;
+            
+
+        }
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+
+                return true;
+
+            }
+
+            return false;
+        }
+
+        private bool InteractWithMovement()
+        {
+            RaycastHit hit;
+            bool hastHit = Physics.Raycast(GetMouseRay(), out hit);
+
+            if (hastHit)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    GetComponent<Mover>().StartMoveAction(hit.point);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+
+}
